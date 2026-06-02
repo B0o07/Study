f595625326e07ae1f17b82e36c3c9aa4aa761385
B0o07 <playssaulo@gmail.com>
2026-06-02 11:29:16 -0300

    Refactor is_raining.py for improved input handling and readability

--- Python/is_raining.py
+++ Python/is_raining.py
@@ -1,20 +1,29 @@
 import time
+out = "Go outside!"
 
-rain = input("Is raining? ").lower()
+while True:
+    rain = input("Is raining? ").lower().strip()
+
+    if rain == "yes" or rain == "no":
+        break
+    
+    else:
+        print("Answer with Yes or No. ")
+        time.sleep(1)
 
 if(rain == "yes"):
     umbrella = input("Do you have an umbrella? ")
 
     if umbrella.lower() == "yes":
-        print("Go outside")
-    
+            print(out)
+            
     elif umbrella.lower() == "no":
         print("Wait the rain to pass.")
 
         while rain.lower() != "no":
             rain = input("Is it still raining? ")
             time.sleep(1)
-        
-        print("Go outside!")
+            
+        print(out)
 else:
-    print("Go outside!")
+    print(out)
